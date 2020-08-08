4a9362103410a89a0f9ba163073cf82cca7418ef
Marcel Bargull <marcel.bargull@udo.edu>
2020-08-08 15:24:20 +0200

    Revert "sketchy: Do not use git cli for fetching"
    This reverts commit 659dd376f31288ed8c3740739558e9874a911b00.

--- recipes/sketchy/build.sh
+++ recipes/sketchy/build.sh
@@ -2,6 +2,9 @@
 set -x
 export RUST_BACKTRACE=full
 
-cargo install -v --locked --root "${PREFIX}" --path .
+# Fix for when some packages use ssh to fetch git repos
+# See https://github.com/rust-lang/cargo/issues/1851#issuecomment-450130685
+CARGO_NET_GIT_FETCH_WITH_CLI=true \
+    cargo install -v --locked --root "${PREFIX}" --path .
 
 "${PYTHON}" -m pip install --no-deps --ignore-installed -vv .
